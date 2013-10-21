78016404b5cf045287fb1b14e487edaed901ea83
AnnaLischen <alsalisbeth@yahoo.com>
2013-10-21 07:37:31 -0700

    Create ALBViewController.h

---
+++ ALBViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALBViewController.h
+//  ArkanoEd
+//
+//  Created by Anastasya Karchevsky on 21.10.13.
+//  Copyright (c) 2013 Rosa Alba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALBViewController : UIViewController <UICollisionBehaviorDelegate>
+
+@end
